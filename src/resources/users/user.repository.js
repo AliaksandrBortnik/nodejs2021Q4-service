b233199f4edbcea3f814e0fd09accb1391793fe9
Alex Bortnik <bortnik.ar@gmail.com>
2021-12-04 23:46:10 +0300

    Fixed user repo

--- src/resources/users/user.repository.js
+++ src/resources/users/user.repository.js
@@ -8,11 +8,13 @@ const getById = async (id) => db.users.find(u => u.id === id);
 const add = async (user) => {
   const newUser = { id: uuidv4(), ...user };
   db.users.push(newUser);
+  return newUser;
 };
 
 const update = async (id, user) => {
   const index = db.users.findIndex(u => u.id === id);
   db.users[index] = { id, ...user };
+  return db.users[index];
 };
 
 const remove = async (id) => {
