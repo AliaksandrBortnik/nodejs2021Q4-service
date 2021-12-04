1e08dcdf2ee49cd501b31ad48bd60e55266a3c63
Alex Bortnik <bortnik.ar@gmail.com>
2021-12-04 22:58:31 +0300

    Implemented User router

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,84 @@
-const router = require('express').Router();
-const User = require('./user.model');
-const usersService = require('./user.service');
+const UserController = require('./user.controller');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+const User = {
+  type: 'object',
+  properties: {
+    id: { type: 'string' },
+    name: { type: 'string' },
+    login: { type: 'string' }
+    // Skipped password
+  }
+}
 
-module.exports = router;
+const getAllOptions = {
+  schema: {
+    response: {
+      200: {
+        type: 'array',
+        items: User
+      }
+    }
+  },
+  handler: UserController.getAll
+}
+
+const getByIdOptions = {
+  schema: {
+    params: {
+      id: { type: 'string' } // TODO: make sure it is UUID
+    },
+    response: {
+      200: User
+    }
+  },
+  handler: UserController.getById
+};
+
+const addOptions = {
+  schema: {
+    response: {
+      201: User
+    }
+  },
+  handler: UserController.add
+};
+
+const updateOptions = {
+  schema: {
+    params: {
+      id: { type: 'string' } // TODO: make sure it is UUID
+    },
+    response: {
+      200: User
+    }
+  },
+  handler: UserController.update
+};
+
+const deleteOptions = {
+  schema: {
+    params: {
+      id: { type: 'string' } // TODO: make sure it is UUID
+    },
+    response: {
+      204: {
+        type: 'object',  // TODO: how to make empty response? Otherwise, add message in response by DELETE request
+        properties: {
+          message: { type: 'string' }
+        }
+      }
+    }
+  },
+  handler: UserController.remove
+};
+
+const userRoutes = (fastify, options, done) => {
+  fastify.get('/users', getAllOptions);
+  fastify.get('/users/:id', getByIdOptions);
+  fastify.post('/users', addOptions);
+  fastify.put('/users/:id', updateOptions);
+  fastify.delete('/users/:id', deleteOptions);
+  done();
+}
+
+module.exports = userRoutes;
