fc86429c0a08d7366cdc411e7e9e05a53049983c
Alex Bortnik <bortnik.ar@gmail.com>
2021-12-04 21:51:09 +0300

    Implemented initial version of UserController

---
+++ src/resources/users/user.controller.js
@@ -0,0 +1,34 @@
+const UserService = require('./user.service');
+
+const getAll = async (_, res) => {
+  const users = await UserService.getAll();
+  res.code(200).send(users);
+}
+
+const getById = async (req, res) => {
+  const user = await UserService.getById(req.params.id);
+  res.code(200).send(user);
+}
+
+const add = async (req, res) => {
+  const user = await UserService.add(req.body);
+  res.status(201).send(user);
+}
+
+const update = async (req, res) => {
+  const user = await UserService.update(req.params.id, req.body);
+  res.code(200).send(user);
+}
+
+const remove = async (req, res) => {
+  await UserService.remove(req.params.id);
+  res.code(204); // TODO: check if need to use empty send()
+}
+
+module.exports = {
+  getAll,
+  getById,
+  add,
+  update,
+  remove
+};
