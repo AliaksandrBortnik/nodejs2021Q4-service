c1cbc9a6445fdb8f809fbd726b44e7b77be1125e
Alex Bortnik <bortnik.ar@gmail.com>
2021-12-05 23:02:37 +0300

    Removed minor comments

--- src/resources/tasks/task.service.js
+++ src/resources/tasks/task.service.js
@@ -1,10 +1,10 @@
 const taskRepo = require("./task.repository");
 
 const getAll = async (boardId) => taskRepo.getAllByBoardId(boardId);
-const getById = async (boardId, id) => taskRepo.getById(id); // TODO: check if boardId is the same in task payload
-const add = async (boardId, task) => taskRepo.add(boardId, task); // TODO: check if boardId is the same in task payload
-const update = async (boardId, taskId, task) => taskRepo.update(taskId, task); // TODO: check if boardId is the same in task payload
-const remove = async (boardId, id) => taskRepo.remove(id); // TODO: check if boardId is the same in task payload
+const getById = async (boardId, id) => taskRepo.getById(id);
+const add = async (boardId, task) => taskRepo.add(boardId, task);
+const update = async (boardId, taskId, task) => taskRepo.update(taskId, task);
+const remove = async (boardId, id) => taskRepo.remove(id);
 
 module.exports = {
   getAll,
