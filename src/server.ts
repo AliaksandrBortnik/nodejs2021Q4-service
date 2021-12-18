9a3e5471d368ccd26ca9a6f51e52a525de690c11
Alex Bortnik <bortnik.ar@gmail.com>
2021-12-19 00:54:39 +0300

    Minor change for handleAppError

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ app.listen(PORT).catch(handleAppError);
  * Log an error as base handle error logic
  * @param error - Error raised by application
  */
-function handleAppError(error: unknown) {
+function handleAppError(error: unknown): void {
   if (error instanceof Error) {
     app.log.error(error.message);
   }
