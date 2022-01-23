ee532d5a41bb912dfe0458dd9bba79ea7e89dd45
Alex Bortnik <bortnik.ar@gmail.com>
2022-01-23 11:20:51 +0300

    Split auth handler as a plugin

--- src/app.ts
+++ src/app.ts
@@ -7,23 +7,21 @@ import {
   FastifyPluginOptions,
   FastifyRegisterOptions
 } from 'fastify';
-import {fastifyJwt} from 'fastify-jwt';
 import {userRouter} from './resources/users/user.router';
 import {boardRouter} from './resources/boards/board.router';
 import {taskRouter} from './resources/tasks/task.router';
 import {logger} from "./logger";
 import "reflect-metadata";
 import {authRouter} from "./resources/auth/auth.router";
-import {config} from "./common/config";
+import {authValidator} from "./auth.plugin";
 
 const app: FastifyInstance = fastify({ logger });
 
-app.register(fastifyJwt, { secret: config.JWT_SECRET_KEY! });
+app.register(authValidator);
 app.register(authRouter);
 app.register(userRouter);
 app.register(boardRouter);
 app.register(taskRouter);
-
 app.register(fastifySwagger as FastifyPluginCallback, <FastifyRegisterOptions<FastifyPluginOptions>>{
   exposeRoute: true,
   routePrefix: '/doc',
