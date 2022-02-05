db4ab350a2bd4edbc6835ccee029ee43727dee6b
Alex Bortnik <bortnik.ar@gmail.com>
2022-02-05 18:01:39 +0300

    Removed native-fastify src

--- src/resources/users/user.request.ts
+++
@@ -1,9 +0,0 @@
-import {FastifyRequest} from "fastify";
-import {User} from "../../entity/user.model";
-
-export type UserFastifyRequest = FastifyRequest<{
-  Params: {
-    id: string
-  },
-  Body: User
-}>;
